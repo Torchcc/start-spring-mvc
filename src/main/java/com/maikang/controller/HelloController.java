47ecaeb1679cad80a26c327e3564df69780416f4
troy <1553765526@qq.com>
2021-02-14 13:06:43 +0800

    first commit

---
+++ src/main/java/com/maikang/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.maikang.controller;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+//@Component
+public class HelloController  {
+    @RequestMapping("/hello")
+    public String sayHello() {
+        System.out.println("hello mvc");
+        return "success";
+    }
+
+
+    @ModelAttribute
+    public void showUser() {
+        System.out.println("inside showUser......");
+    }
+}
